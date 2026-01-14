5ce546f42b9e23d3e3daf90629b44b6e0d531ef1
Franck S. <franck.spadotto@yahoo.com>
2026-01-14 14:18:36 +0100

    fix: change tabs into spaces

--- TypeScript/task_0/js/main.ts
+++ TypeScript/task_0/js/main.ts
@@ -1,26 +1,26 @@
 // Init student interface
 interface Student {
-	firstName: string,
-	lastName: string,
-	age: number,
-	location: string
+  firstName: string,
+  lastName: string,
+  age: number,
+  location: string
 }
 
 // Create student 1
 const student1: Student = {
-	firstName: "Jojo",
-	lastName: "Lebigorneau",
-	age: 75,
-	location: "Monjardin"
+  firstName: "Jojo",
+  lastName: "Lebigorneau",
+  age: 75,
+  location: "Monjardin"
 }
 
 
 // Create student 2
 const student2: Student = {
-	firstName: "Nono",
-	lastName: "Lasticot",
-	age: 12,
-	location: "Materasse"
+  firstName: "Nono",
+  lastName: "Lasticot",
+  age: 12,
+  location: "Materasse"
 }
 
 // Store students in a list
@@ -28,8 +28,8 @@ const studentList: Student[] = [student1, student2];
 
 // Define columns
 const columns: {key: keyof Student; label: string}[] = [
-	{key: "firstName", label: "First name"},
-	{key: "location", label: "Location"},
+  {key: "firstName", label: "First name"},
+  {key: "location", label: "Location"},
 ];
 
 // Create table with header
@@ -39,22 +39,22 @@ const headerRow: HTMLTableRowElement = header.insertRow();
 
 // Add header cells
 columns.forEach(({ label }) => {
-	const th: HTMLTableCellElement = document.createElement("th");
-	th.textContent = label;
-	th.className = "header-cell";
-	headerRow.appendChild(th);
+  const th: HTMLTableCellElement = document.createElement("th");
+  th.textContent = label;
+  th.className = "header-cell";
+  headerRow.appendChild(th);
 })
 
 // Populate table with data
 const tbody = table.createTBody();
 
 studentList.forEach(( student ) => {
-	const row: HTMLTableRowElement = tbody.insertRow();
+  const row: HTMLTableRowElement = tbody.insertRow();
 
-	columns.forEach(({ key }) => {
-		const cell = row.insertCell();
-		cell.textContent = String(student[key]);
-	});
+  columns.forEach(({ key }) => {
+    const cell = row.insertCell();
+    cell.textContent = String(student[key]);
+  });
 });
 
 document.body.appendChild(table);
